fbd3e5738e93792a98c83f15152decad316068ea
jacob-ebey <jacob-ebey@users.noreply.github.com>
2020-12-24 23:11:51 +0000

    deploy: 36456a56d1992efd230c8c6ea4f34c586da28591

---
+++ static/732.4d84504a8796fcd3e852.js
@@ -0,0 +1,3 @@
+(self.webpackChunk_pwrc_example=self.webpackChunk_pwrc_example||[]).push([[732],{732:(l,e,t)=>{"use strict";t.r(e),t.d(e,{default:()=>n});var a=t(473),u=t(305);function n(){return(0,u.ZQ)("About"),(0,u.jq)({name:"description",content:"PWRC example about page"}),(0,a.h)("h1",null,"About Page :D")}}}]);
+
+//# sourceMappingURL=732.4d84504a8796fcd3e852.js.map
